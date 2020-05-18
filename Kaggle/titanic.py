b9717cc7ecbfc06f672b1e71452062c55769699a
songchan <songchanback@gmail.com>
2020-05-18 18:02:14 +0900

    20200518

--- Kaggle/titanic.py
+++ Kaggle/titanic.py
@@ -127,80 +127,150 @@ pd.crosstab([data.SibSp],data.Survived).style.background_gradient(cmap='summer_r
 
 
 # %%
-f,ax = plt.subplots(1,2, figsize=20,8))
-sns.barplot('SibSp',)
+f, ax = plt.subplots(1,2,figsize=(20,8))
+sns.barplot('SibSp','Survived',data=data,ax=ax[0])
+ax[0].set_title('SibSp vs Survived')
+sns.factorplot('SibSp','Survived',data=data,ax=ax[1])
+ax[1].set_title('SibSp vs Survived')
+# %%
+pd.crosstab(data.SibSp, data.Pclass).style.background_gradient(cmap='summer_r')
+# %%
+f, ax = plt.subplots(1,2,figsize=(20,8))
+sns.barplot('Parch','Survived',data=data,ax=ax[0])
+ax[0].set_title("Parch vs Survived")
+sns.factorplot('Parch','Survived',data=data,ax=ax[1])
+ax[1].set_title("Parch vs Survived")
+plt.close(2)
+plt.show()
+# %%
+print("Hightest Fare was : ",data['Fare'].max())
+print("Lowest Fare was : ", data['Fare'].min())
+print("Average Fare was:",data['Fare'].mean())
+
+# %%
+f,ax = plt.subplots(1,3,figsize=(20,8))
+sns.distplot(data[data['Pclass']==1].Fare,ax=ax[0])
+ax[0].set_title('Fares in Pclass 1')
+sns.distplot(data[data['Pclass']==2].Fare,ax=ax[1])
+ax[1].set_title('Fares in Pclass 2')
+sns.distplot(data[data['Pclass']==3].Fare,ax=ax[2])
+ax[2].set_title('Fares in Pclass 3')
+
+# %%
+sns.heatmap(data.corr(),annot=True, cmap='RdYlGn',linewidths=0.2)
+fig=plt.gcf()
+fig.set_size_inches(10,8)
+plt.show()
+# %%
+data['Age_band']=0
+data.loc[data['Age']<=16,'Age_band']=0
+data.loc[(data['Age']>16)&(data['Age']<=32),'Age_band']=1
+data.loc[(data['Age']>32)&(data['Age']<=48),'Age_band']=2
+data.loc[(data['Age']>48)&(data['Age']<=64),'Age_band']=3
+data.loc[data['Age']>64, 'Age_band']=4
+data.head(2)
+# %%
+data['Age_band'].value_counts().to_frame().style.background_gradient(cmap='summer')
+
+# %%
+sns.factorplot('Age_band','Survived',data=data,col='Pclass')
+
+# %%
+data['Family_Size']=0
+data['Family_Size']=data['Parch']+data['SibSp']
+data['Alone']=0
+data.loc[data.Family_Size==0,'Alone']=1
+data.head()
+
+# %%
+f,ax = plt.subplots(1,2,figsize=(18,6))
+sns.factorplot('Family_Size','Survived',data=data,ax=ax[0])
+ax[0].set_title('Family_Size vs Survived')
+sns.factorplot('Alone','Survived',data=data,ax=ax[1])
+ax[1].set_title("Alone vs Survived")
+plt.close(2)
+plt.close(3)
+plt.show()
+
+# %%
+sns.factorplot('Alone','Survived',data=data,hue='Sex',col='Pclass')
+
+# %%
+data['Fare_Range']=pd.qcut(data['Fare'],4)
+data.head()
+data.groupby(['Fare_Range'])['Survived'].mean().to_frame().style.background_gradient(cmap='summer_r')
+# %%
+data['Fare_cat']=0
+data.loc[data['Fare']<=7.91,'Fare_cat']=0
+data.loc[(data['Fare']>7.91)&(data['Fare']<=14.454),'Fare_cat']=1
+data.loc[(data['Fare']>14.454)&(data['Fare']<=31),'Fare_cat']=2
+data.loc[(data['Fare']>31)&(data['Fare']<=513),'Fare_cat']=3
+# %%
+sns.factorplot('Fare_cat',"Survived",data=data,hue='Sex')
 
 # %%
 
+data['Sex'].replace(['male','female'],[0,1],inplace=True)
 
 # %%
 
+data['Embarked'].replace(['S','C','Q'],[0,1,2],inplace=True)
+data['Initial'].replace(['Mr','Mrs','Miss','Master','Other'],[0,1,2,3,4],inplace=True)
 
 # %%
-
+data.drop(['Name','Age','Ticket','Fare','Cabin','Fare_Range','PassengerId'],axis=1,inplace=True)
+sns.heatmap(data.corr(),annot=True,cmap='RdYlGn',linewidths=0.2,annot_kws={'size':20})
+fig=plt.gcf()
+fig.set_size_inches(18,15)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.show()
+# %%
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.metrics import confusion_matrixs
 
 # %%
-
-
-# %%
-
-
-# %%
-
-
-# %%
-
+train,test = train_test_split(data, test_size=0.3, random_state=0,stratify=data['Survived'])
+train_X = train[train.columns[1:]]
+train_Y = train[train.columns[:1]]
+test_X = test[test.columns[1:]]
+test_Y = test[test.columns[:1]]
+X=data[data.columns[1:]]
+Y=data['Survived']
 
 # %%
-
-
-# %%
-
-
-# %%
-
+model=svm.SVC(kernel='rbf',C=1,gamma=0.1)
+model.fit(train_X,train_Y)
+prediction1=model.predict(test_X)
+print('Accuracy for rbf SVM is ',metrics.accuracy_score(prediction1,test_Y))
 
 # %%
-
-
-# %%
-
-
-# %%
-
+model=svm.SVC(kernel='linear', C=0.1, gamma=0.1)
+model.fit(train_X,train_Y)
+prediction2=model.predict(test_X)
+print('Accuracy for linear SVm is',metrics.accuracy_score(prediction2, test_Y))
 
 # %%
-
-
-# %%
-
-
-# %%
-
+model = LogisticRegression()
+model.fit(train_X, train_Y)
+prediction3=model.predict(test_X)
+print('The Accuracy of the Logistic Regression is ',metrics.accuracy_score(prediction3,test_Y))
 
 # %%
-
-
-# %%
-
-
-# %%
-
+model = DecisionTreeClassifier()
+model.fit(train_X, train_Y)
+prediction4=model.predict(test_X)
+print('The Accuracy of the Decision Tree is',metrics.accuracy_score(prediction4,test_Y))
 
 # %%
-
-
-# %%
-
-
-# %%
-
-
-# %%
-
-
-# %%
-
+model=KNeighborsClassifier
 
 # %%
 
