e0d0633cced7a3dc7ebb0a8b62297ecd949ba2f5
bsch0111 <bsch0111@gmail.com>
2020-05-29 23:57:25 +0900

    0529 저녁 chromaticity 폴더 생성해야 실행됨

--- conEDA/chromaticity/1chromaticity.py
+++ conEDA/chromaticity/1chromaticity.py
@@ -6,29 +6,42 @@ import os
 
 
 # %%
-base_dir = '/home/backsongchan/Chromaticity'
-
-# %%
+base_dir = 'C:/bsch0111/Chromaticity'
+# 최종 결과물 속성
+# point, L, a*, b*
+#%%
+# Chromaticity 파일리스트
 files=os.listdir(base_dir)
-
+# 포인트 배열
+points = []
+# %%
+# 포인트 추출
+for file in files:
+    point = file[-21:-18]
+    points.append(point)
+# %%
+# 데이터프레임 생성
+# point 열을 인덱스화
+chromaticity_df = pd.DataFrame()
+chromaticity_df['point'] = points
+chromaticity_df.set_index('point')
 # %%
 
+# %%
+p_df = pd.DataFrame(columns=['Sample Name','L', 'a*','b*'])
+for file in files:
+    path = os.path.join(base_dir,file)
+    # 데이터 프레임에 행 추가
+    p_df = p_df.append(pd.read_excel(path, sheet_name='Sheet1'),ignore_index=True) 
 
 # %%
-
-
+chromaticity_df['L'] = p_df['L']
+chromaticity_df['a*'] = p_df['a*']
+chromaticity_df['b*'] = p_df['b*']
 # %%
-
-
-# %%
-
-
-# %%
-
-
-# %%
-
-
+import datetime
+basename = os.getcwd()+'\\Chromaticity\\Chromaticity'+datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+chromaticity_df.to_csv(basename+'_Chromaticity.csv')
 # %%
 
 
