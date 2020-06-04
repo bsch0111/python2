0f39017f8c933633d8bcf5b133464e9f58597e2f
songchan <songchanback@gmail.com>
2020-06-04 09:35:07 +0900

    0603 매칭 포인트 추출 성공

--- conEDA/chromaticity/1chromaticity.py
+++ conEDA/chromaticity/1chromaticity.py
@@ -1,9 +1,10 @@
 #%%
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import os
  # %%
-base_dir = "C:/inputcsv/20200514/mother1/3.2.2 Chromaticity"
+base_dir = "C:/inputcsv/painting/conUNSOUNG/FAMILY(PAI, UN-SOUNG)/3. Material Analysis/3.2 Basic Analysis/3.2.2 Chromaticity"
 # 최종 결과물 속성
 # point, L, a*, b*
 #%%
@@ -34,15 +35,8 @@ for file in files:
 chromaticity_df['L'] = p_df['L']
 chromaticity_df['a*'] = p_df['a*']
 chromaticity_df['b*'] = p_df['b*']
-#chromaticity_df = chromaticity_df.set_index('point')
 #index를 point로 여기서 하면 추후에 squreform할 때 index로 사용할 컬럼이 부족
-# %%
-
-'''
-import datetime
-basename = os.getcwd()+'\\Chromaticity\\Chromaticity'+datetime.datetime.now().strftime("%y%m%d_%H%M%S")
-chromaticity_df.to_csv(basename+'_Chromaticity.csv')
-'''
+#chromaticity_df = chromaticity_df.set_index('point')
 
 # %%
 # 포인트 간의 거리를 squareform으로 출력
@@ -56,24 +50,25 @@ sns.heatmap(df_dist, cmap = 'RdYlGn', linewidths = 0.2)
 # %%
 # chromaticity 거리가 3 아래인 것들을 추출
 chromaticity_3under = df_dist[(df_dist<3) & (df_dist != 0)]
-# %%
-
-import datetime
-basename = os.getcwd()+'\\Chromaticity\\Chromaticity_3under'+datetime.datetime.now().strftime("%y%m%d_%H%M%S")
-chromaticity_3under.to_csv(basename+'_Chromaticity.csv')
 
 # %%
 chromaticity_3under_match = pd.DataFrame(columns=['match'],index=chromaticity_3under.columns)
 
 # %%
 for co in chromaticity_3under.columns:
-    chromaticity_3under_match.loc[co,'match'] = chromaticity_3under[chromaticity_3under[co] > 0].index
+    matching = []
+    # Series 나 Dataframe의 데이터를 조회할 때 dtype과 Name를 출력하고 싶지 않다면, 배열에 넣은후 출력하는 방법 사용 
+    for var in (chromaticity_3under[chromaticity_3under[co] > 0].index) :
+        matching.append(var)
+    chromaticity_3under_match.loc[co,'match'] = matching
 # %%
-chromaticity_3under_match['match'].apply(str)
-
 import datetime
-basename = os.getcwd()+'\\Chromaticity\\Chromaticity_3under_match'+datetime.datetime.now().strftime("%y%m%d_%H%M%S")
-chromaticity_3under_match.to_csv(basename+'_Chromaticity.csv')
+basename1 = os.getcwd()+'\\Chromaticity\\Chromaticity_3under_match'+datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+basename2 = os.getcwd()+'\\Chromaticity\\Chromaticity_heatmap'+datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+
+chromaticity_3under_match.to_csv(basename1+'_Chromaticity.csv')
+#plt = sns.get_figure()
+#plt.savefig(basename2 +'_output.png')
 
 #%%
 #정규표현식을 이용한 삭제
@@ -89,3 +84,10 @@ for co in chromaticity_3under.columns:
     chromaticity_3under_match.loc[co,'match_point'] = re.sub('[a-zA-Z]',' ',chromaticity_3under_match[co]).strip()
 '''
 
+# %%
+# 데이터 output 저장용 코드
+'''
+import datetime
+basename = os.getcwd()+'\\Chromaticity\\Chromaticity_3under'+datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+chromaticity_3under.to_csv(basename+'_Chromaticity.csv')
+'''
